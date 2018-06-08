8c4bc60e014909aab0b1cce66c40939f2fca8a74
How Chen <how.chen@gemalto.com>
2018-06-08 15:57:06 +0800

    change function parm to ptr, add device attribute get

--- linux_serial_program.c
+++ linux_serial_program.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "serial_api.h"
 
 int main(int argc,char** argv)
 {
-    portObj_t portObj;
+    portObj_t *portObj = NULL;
     CommErrorCode_e res;
 
-    res = comm_Get_Opt(&portObj, argc, argv);
+    portObj = (portObj_t *)malloc(sizeof(portObj_t));
+    memset(portObj, 0x0, sizeof(portObj_t));
+
+    res = comm_Get_Opt(portObj, argc, argv);
     if(res != COMM_PORT_OK)
     {
         printf("device parm get fail!\n");
+        free(portObj);
         return res;
     }
 
@@ -17,14 +23,28 @@ int main(int argc,char** argv)
     if(res != COMM_PORT_OK)
     {
         printf("device open fail!\n");
+        free(portObj);
+        return res;
     }
+
+    res = comm_Get_Port_Atrribute(portObj);
+    if(res != COMM_PORT_OK)
+    {
+        printf("device attribute get fail fail!\n");
+        free(portObj);
+        return res;
+    }
+
 
     res = comm_Close_Port(portObj);
     if(res != COMM_PORT_OK)
     {
         printf("device close fail!\n");
+        free(portObj);
+        return res;
     }
 
+    free(portObj);
     return 0;
 }
 
